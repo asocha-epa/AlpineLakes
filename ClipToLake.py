cc7f0e927b1c3aa40b3b7828af807f1382f4e0e9
asocha-epa <socha.annika@epa.gov>
2023-07-26 09:09:00 -0400

    create file

---
+++ ClipToLake.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 09:07:35 2023
+
+@author: ASOCHA
+
+For analyzing surface temperature of lakes within a buffered region that delineates the lake - starts with buffering the lake area then
+clipping the LST raster to the lake
+"""
+
