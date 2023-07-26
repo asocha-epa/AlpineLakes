2e53fb5e49790ae22a5f1c52878720726a272e42
asocha-epa <socha.annika@epa.gov>
2023-07-26 09:18:46 -0400

    Create file

---
+++ EROS_M2M_LandsatDownload.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 09:07:35 2023
+
+@author: ASOCHA
+
+Building on a sample script provided by the USGS for downloading data from their M2M API to download landsat ard surface temperature data
+"""
+
