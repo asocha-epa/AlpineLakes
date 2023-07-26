0adac02c1b95560e5a27f5df6d3e9c0bc971ac50
asocha-epa <socha.annika@epa.gov>
2023-07-26 15:39:11 -0400

    create buffered lake boundary

--- iterateTest.py
+++ iterateTest.py
@@ -11,11 +11,28 @@ import os
 import tkinter as tk
 from tkinter.filedialog import askdirectory
 import rasterio as rio
+import geopandas as gpd
 
 #get rid of root window
 root = tk.Tk()
 root.withdraw()
 
+#read in shapefile - will need to update later for getting geometry from gpkg
+input_df = gpd.read_file(r'C:\Users\asocha\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Alpine Lakes\Tahoe_Soils_and_Hydro_Data\Tahoe_Soils_and_Hydro_Data.shp')
+
+# Reading the input CRS. 
+input_crs = input_df.crs
+
+#reproject if necessary to match rasters which are Albers Equal Area
+if inpus_crs != 'epsg:9822':
+    input_df = input_df.to_crs('epsg:9822')
+
+# Create buffer and set as new geometry
+input_df['buffer'] = input_df.buffer(-100)
+buff_df = input_df.drop(columns=['geometry']).set_geometry('buffer')
+
+
+#%%
 #ask user to select folder for directory
 print('select folder', "\n")
 wd = askdirectory(title = 'Select Directory Folder')
@@ -37,10 +54,15 @@ for folder in os.listdir(wd):
                     if file.endswith('ST_B10.TIF') or file.endswith('ST_B6.TIF'):
                         ST_band = file
                     
-                    #read in raster bands as arrays
-                    with rio.open(ST_band) as src:
-                        surfTemp = src.read()
-                        profile = src.profile
-        
-                    with rio.open(QA_band) as src:
-                        QA = src.read()
+                        #read in raster bands as arrays
+                        with rio.open(ST_band) as src:
+                            surfTemp = src.read()
+                            profile = src.profile
+            
+                        with rio.open(QA_band) as src:
+                            QA = src.read()
+                        
+                        
+            break
+        break
+    break
