1c0e8cbdc3f3cb51d908417b9a9b6fd35ded60b0
asocha-epa <socha.annika@epa.gov>
2023-07-26 13:40:59 -0400

    get ST and QA bands

--- iterateTest.py
+++ iterateTest.py
@@ -18,7 +18,8 @@ root.withdraw()
 #ask user to select folder for directory
 print('select folder', "\n")
 wd = askdirectory(title = 'Select Directory Folder')
-                  
+
+#go through each folder and subfolder in the root folder to get raster files, skipping the tar zip files              
 for folder in os.listdir(wd):
     rootDir = os.path.join(wd, folder)
     for root, subDirs, files in os.walk(rootDir):
@@ -28,3 +29,9 @@ for folder in os.listdir(wd):
             if subDir.endswith('.tar'):
                 continue
             else:
+                #get the QA and the surface temperature bands
+                for file in os.listdir('.'):
+                    if file.endswith('QA_PIXEL.TIF'):
+                        QA_band = file
+                    if file.endswith('ST_B10.TIF') or file.endswith('ST_B6.TIF'):
+                        ST_band = file
