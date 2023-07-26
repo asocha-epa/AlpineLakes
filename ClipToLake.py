7f2bb7b197c409f09fdee96bea4539b436ec6258
asocha-epa <socha.annika@epa.gov>
2023-07-26 09:11:10 -0400

    clip raster to buffered geometry

--- ClipToLake.py
+++ ClipToLake.py
@@ -29,3 +29,32 @@ buff_df = buff_df.to_crs(input_crs)
 
 # Exporting the output to a shapefile
 #buff_df.to_file(r'C:\Users\asocha\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Alpine Lakes\Tahoe_Soils_and_Hydro_Data\Tahoe_Soils_and_Hydro_Data_100m_bufferIn.shp')
+
+#%%
+#read in the ST band and get its crs
+src =  rio.open(r'C:\Users\ASOCHA\OneDrive - Environmental Protection Agency (EPA)\Profile\Documents\Alpine Lakes\LandsatData\LC08_CU_003008_20210618_20210702_02_ST_B10.TIF')
+src_crs = src.crs
+
+#project lake df to the same crs
+buff_df = buff_df.to_crs(src.crs)
+
+#from https://gist.github.com/mhweber/1af47ef361c3b20184455060945ac61b
+def getFeatures(gdf):
+    """Function to parse features from GeoDataFrame in such a manner that rasterio wants them"""
+    import json
+    return [json.loads(gdf.to_json())['features'][0]['geometry']]
+
+coords = getFeatures(buff_df)
+
+clipped_array, clipped_transform = mask(dataset=src, shapes=coords, crop=True)
+
+out_meta = src.meta.copy()
+out_meta.update({"driver": "GTiff",
+                 "height": clipped_array.shape[1],
+                 "width": clipped_array.shape[2],
+                 "transform": clipped_transform})
+
+out_tif= "clipped_example.tif"
+
+with rio.open(out_tif, "w", **out_meta) as dest:
+    dest.write(clipped_array)
