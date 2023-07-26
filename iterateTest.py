8ae0fec80c76ca24055b381d908bcfb694e72793
asocha-epa <socha.annika@epa.gov>
2023-07-26 13:29:47 -0400

    create file

---
+++ iterateTest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 26 13:27:16 2023
+
+@author: ASOCHA
+
+Initial testing for combining lake temperature data processing in one script and iterating the process for all files downloaded
+Working to make additional scripts into functions and modules that can be imported for a cleaner script
+"""
+
